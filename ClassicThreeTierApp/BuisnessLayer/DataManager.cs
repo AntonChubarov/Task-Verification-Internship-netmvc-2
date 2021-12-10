a42b8db0638d4846f8994f7a18175f059105d82e
Anton Chubarov <achlp600@gmail.com>
2021-12-10 15:19:04 +0200

    feat: buisness layer

---
+++ ClassicThreeTierApp/BuisnessLayer/DataManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuisnessLayer.Interfaces;
+
+namespace BuisnessLayer
+{
+    class DataManager
+    {
+        private IDyrectorysRepository _dyrectorysRepository;
+        private IMaterialsRepository _materialsRepository;
+        
+        public DataManager(IDyrectorysRepository dyrectorysRepository, IMaterialsRepository materialsRepository)
+        {
+            _dyrectorysRepository = dyrectorysRepository;
+            _materialsRepository = materialsRepository;
+        }
+    }
+}
---
+++ ClassicThreeTierApp/BuisnessLayer/Implementation/DyrectorysRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BuisnessLayer.Interfaces;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuisnessLayer.Implementation
+{
+    class DyrectorysRepository : IDyrectorysRepository
+    {
+        private PosgreContext _context;
+
+        public DyrectorysRepository(PosgreContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteDirectory(Directory acheive)
+        {
+            _context.Directories.Remove(acheive);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Directory> GetAllDirectories(bool includeMaterials = false)
+        {
+            var directories = _context.Directories.ToList<Directory>();
+            if (!includeMaterials)
+            {
+                foreach (Directory directory in directories)
+                {
+                    directory.Materials = null;
+                }
+            }
+            return directories;
+        }
+
+        public Directory GetDyrectoryById(int directoryId, bool includeMaterials = false)
+        {
+            var directory = _context.Directories.Find(directoryId);
+            if (!includeMaterials)
+            {
+                directory.Materials = null;
+            }
+            return directory;
+        }
+
+        public void SaveDirectory(Directory acheive)
+        {
+            _context.Entry(acheive).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
